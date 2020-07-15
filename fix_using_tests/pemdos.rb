2cfdd964b0b042a93bc05e39f93dd99e6c3827e1
jade decordova <lacroixjaded@gmail.com>
2020-07-15 19:17:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-  binding.pry
-    10 * "s" + string
+  add_s = 0
+  until add_s == 10
+    "s" + string
+    add_s += 1
   else
     string
   end
